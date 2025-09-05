03a2c8309d1aac0eb618755ad0ec63e34573ca82
Simon <simon.nicholas@outlook.com>
2025-09-05 13:03:11 +1000

    removed users route

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,11 @@ const express = require('express');
 const apiRouter = express.Router({mergeParams: true});
 
 const itemsRouter = require('./items');
-const usersRouter = require('./users');
 const cartRouter = require('./cart');
 const purchasesRouter = require('./purchases');
 const reviewsRouter = require('./reviews');
 
 apiRouter.use('/items', itemsRouter);
-apiRouter.use('/users', usersRouter);
 apiRouter.use('/cart', cartRouter);
 apiRouter.use('/purchases', purchasesRouter);
 apiRouter.use('/reviews', reviewsRouter)
