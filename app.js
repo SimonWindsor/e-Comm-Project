52cf3995d71d11418178acbddb81bf42bd5cb344
Simon <simon.nicholas@outlook.com>
2025-11-08 20:35:09 +1000

    Update app.js

--- app.js
+++ app.js
@@ -199,6 +199,5 @@ app.use(notFoundHandler);
 // Global error handler (must be last)
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Express server started at port 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
