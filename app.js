38c6d003dc300abe1c4bf18698ab193ded59c907
Simon <simon.nicholas@outlook.com>
2025-11-08 17:46:18 +1000

    changing the port again to see if I get same problem

--- app.js
+++ app.js
@@ -233,6 +233,7 @@ app.use(notFoundHandler);
 // Global error handler (must be last)
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Express server started at port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+console.log(`Express server started at port ${PORT}`);
 });
